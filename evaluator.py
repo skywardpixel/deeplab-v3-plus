98192dd33afa56434ef2596bc6e4f80496a3e6e7
Kyle Yan <kyleyan@cs.uw.edu>
2018-12-01 16:49:09 -0800

    :construction: Migrate data loading and training from original repo

---
+++ evaluator.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class Evaluator(object):
+    def __init__(self, num_class):
+        self.num_class = num_class
+        self.confusion_matrix = np.zeros((self.num_class,) * 2)
+
+    def pixel_accuracy(self):
+        acc = np.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
+        return acc
+
+    def pixel_accuracy_class(self):
+        acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
+        acc = np.nanmean(acc)
+        return acc
+
+    def mean_iou(self):
+        iou = np.diag(self.confusion_matrix) / (
+                np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
+                np.diag(self.confusion_matrix))
+        iou = np.nanmean(iou)
+        return iou
+
+    def frequency_weighted_iou(self):
+        freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
+        iu = np.diag(self.confusion_matrix) / (
+                np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
+                np.diag(self.confusion_matrix))
+
+        fw_iou = (freq[freq > 0] * iu[freq > 0]).sum()
+        return fw_iou
+
+    def _generate_matrix(self, gt_image, pre_image):
+        mask = (gt_image >= 0) & (gt_image < self.num_class)
+        label = self.num_class * gt_image[mask].astype('int') + pre_image[mask]
+        count = np.bincount(label, minlength=self.num_class ** 2)
+        confusion_matrix = count.reshape(self.num_class, self.num_class)
+        return confusion_matrix
+
+    def add_batch(self, gt_image, pre_image):
+        assert gt_image.shape == pre_image.shape
+        self.confusion_matrix += self._generate_matrix(gt_image, pre_image)
+
+    def reset(self):
+        self.confusion_matrix = np.zeros((self.num_class,) * 2)
---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DATASET_ROOT = os.path.join(PROJECT_ROOT, 'data')
